6429c4607bbc7e6031ddd912c0ab0445a6361ca8
Yezid <ygm.mecatronico@gmail.com>
2025-08-20 10:36:10 -0500

    DataSeed o Datos Iniciales

--- Context/ProjectContext.cs
+++ Context/ProjectContext.cs
@@ -20,14 +20,49 @@ public class ProjectefContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        List<Categoria> categoriasInit = new List<Categoria>();
+        categoriasInit.Add(new Categoria()
+        {
+            CategoriaId = Guid.Parse("3b8b7607-3ef4-4820-8e68-dacc4ea2d93a"),
+            Nombre = "Actividades Pendientes",
+            Peso = 20            
+        });
+        categoriasInit.Add(new Categoria()
+        {
+            CategoriaId = Guid.Parse("3b8b7607-3ef4-4820-8e68-dacc4ea2d902"),
+            Nombre = "Actividades Personales",
+            Peso = 50
+            
+        });
+
         modelBuilder.Entity<Categoria>(categoria =>
         {
             categoria.ToTable("Categoria");
             categoria.HasKey(p => p.CategoriaId);
             categoria.Property(p => p.Nombre).IsRequired().HasMaxLength(150);
-            categoria.Property(p => p.Descripcion);
+            categoria.Property(p => p.Descripcion).IsRequired(false);
             categoria.Property(p => p.Peso);
 
+            categoria.HasData(categoriasInit);
+        });
+
+
+        List<Tarea> tareasInit = new List<Tarea>();
+        tareasInit.Add(new Tarea()
+        {
+            TareaId = Guid.Parse("3b8b7607-3ef4-4820-8e68-dacc4ea2d910"),
+            CategoriaId = Guid.Parse("3b8b7607-3ef4-4820-8e68-dacc4ea2d93a"),
+            PrioridadTarea = Prioridad.Media,
+            Titulo = "Pago de Servicios Publicos",
+            FechaCreacion = DateTime.Now
+        });
+        tareasInit.Add(new Tarea()
+        {
+            TareaId = Guid.Parse("3b8b7607-3ef4-4820-8e68-dacc4ea2d920"),
+            CategoriaId = Guid.Parse("3b8b7607-3ef4-4820-8e68-dacc4ea2d902"),
+            PrioridadTarea = Prioridad.Baja,
+            Titulo = "Terminar de ver pelicula en Netflix",
+            FechaCreacion = DateTime.Now
         });
 
         modelBuilder.Entity<Tarea>(tarea =>
@@ -36,12 +71,14 @@ public class ProjectefContext : DbContext
             tarea.HasKey(p => p.TareaId);
             tarea.HasOne(p => p.Categoria).WithMany(p => p.Tareas).HasForeignKey(p => p.CategoriaId);
             tarea.Property(p => p.Titulo).IsRequired().HasMaxLength(200);
-            tarea.Property(p => p.Descripcion);
+            tarea.Property(p => p.Descripcion).IsRequired(false);
             tarea.Property(p => p.PrioridadTarea);
             tarea.Property(p => p.FechaCreacion);
             tarea.Ignore(p => p.Resumen);
             tarea.Property(p => p.Puntos);
             tarea.Property(p => p.FechaLimite);
+            
+            tarea.HasData(tareasInit);
         });
     }
     
---
+++ Migrations/20250820153042_InitialData.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace projectef.Migrations
+{
+    public partial class InitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Tarea",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Categoria",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "Categoria",
+                columns: new[] { "CategoriaId", "Descripcion", "Nombre", "Peso" },
+                values: new object[] { new Guid("3b8b7607-3ef4-4820-8e68-dacc4ea2d902"), null, "Actividades Personales", 50 });
+
+            migrationBuilder.InsertData(
+                table: "Categoria",
+                columns: new[] { "CategoriaId", "Descripcion", "Nombre", "Peso" },
+                values: new object[] { new Guid("3b8b7607-3ef4-4820-8e68-dacc4ea2d93a"), null, "Actividades Pendientes", 20 });
+
+            migrationBuilder.InsertData(
+                table: "Tarea",
+                columns: new[] { "TareaId", "CategoriaId", "Descripcion", "FechaCreacion", "FechaLimite", "PrioridadTarea", "Puntos", "Titulo" },
+                values: new object[] { new Guid("3b8b7607-3ef4-4820-8e68-dacc4ea2d910"), new Guid("3b8b7607-3ef4-4820-8e68-dacc4ea2d93a"), null, new DateTime(2025, 8, 20, 10, 30, 42, 382, DateTimeKind.Local).AddTicks(4612), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, 0, "Pago de Servicios Publicos" });
+
+            migrationBuilder.InsertData(
+                table: "Tarea",
+                columns: new[] { "TareaId", "CategoriaId", "Descripcion", "FechaCreacion", "FechaLimite", "PrioridadTarea", "Puntos", "Titulo" },
+                values: new object[] { new Guid("3b8b7607-3ef4-4820-8e68-dacc4ea2d920"), new Guid("3b8b7607-3ef4-4820-8e68-dacc4ea2d902"), null, new DateTime(2025, 8, 20, 10, 30, 42, 382, DateTimeKind.Local).AddTicks(4627), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, 0, "Terminar de ver pelicula en Netflix" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Tarea",
+                keyColumn: "TareaId",
+                keyValue: new Guid("3b8b7607-3ef4-4820-8e68-dacc4ea2d910"));
+
+            migrationBuilder.DeleteData(
+                table: "Tarea",
+                keyColumn: "TareaId",
+                keyValue: new Guid("3b8b7607-3ef4-4820-8e68-dacc4ea2d920"));
+
+            migrationBuilder.DeleteData(
+                table: "Categoria",
+                keyColumn: "CategoriaId",
+                keyValue: new Guid("3b8b7607-3ef4-4820-8e68-dacc4ea2d902"));
+
+            migrationBuilder.DeleteData(
+                table: "Categoria",
+                keyColumn: "CategoriaId",
+                keyValue: new Guid("3b8b7607-3ef4-4820-8e68-dacc4ea2d93a"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Tarea",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Categoria",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
