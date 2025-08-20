8b541d4de81dff6ff00a5bcc5979371496ca6be6
Yezid <ygm.mecatronico@gmail.com>
2025-08-19 19:21:59 -0500

    Datanotations en los atributos a los modelos

--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace projectef.Models;
 
 public class Tarea
 {
+    [Key]
     public Guid TareaId { get; set; }
-    public Guid CategoriaId{ get; set; }
 
+    [ForeignKey("CategoriaId")]
+    public Guid CategoriaId { get; set; }
+
+    [Required]
+    [MaxLength(200)]
     public string Titulo { get; set; }
 
     public string Descripcion { get; set; }
@@ -13,9 +21,10 @@ public class Tarea
 
     public DateTime FechaCreacion { get; set; }
 
-public virtual Categoria Categoria { get; set; }
+    public virtual Categoria Categoria { get; set; }
 
-    
+    [NotMapped]
+    public string Resumen { get; set; }
 }
 
 public enum Prioridad
