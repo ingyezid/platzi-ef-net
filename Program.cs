81a9aac0a57ebc891436adc1a0f89208a62eb8a7
Yezid <ygm.mecatronico@gmail.com>
2025-08-19 19:34:39 -0500

    Base de datos en memoria

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
+using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using projectef.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ProjectefContext>(p => p.UseInMemoryDatabase("ProjectEF"));
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async ([FromServices] ProjectefContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+}); 
 
 app.Run();
